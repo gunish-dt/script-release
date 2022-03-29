cec11e201790982e7b1f95edf32e2092794d7160
gunish-dt <gunish@devtron.ai>
2022-03-29 09:59:17 +0530

    script with hyperion

--- post-build-new.sh
+++ post-build-new.sh
@@ -1,7 +1,10 @@
 APP_DOCKER_REPO=$DOCKER_REPO
 APP_MANIFEST=$MANIFEST
+HYPERION_APP_MANIFEST="values.yaml"
 REPO=$REPOSITORY
 GIT_REPO="github.com/$REPO.git"
+HYPERION_REPO=$HYPERION_REPOSITORY
+HYPERION_GIT_REPO="github.com/$HYPERION_REPO.git"
 GIT_CONFIG_EMAIL=$GIT_CONFIG_EMAIL
 GIT_CONFIG_NAME=$GIT_CONFIG_NAME
 GIT_USERNAME=$GIT_USERNAME
@@ -13,7 +16,10 @@ RELEASE_BRANCH=$RELEASE_BRANCH
 MIGRATOR_FILE=$MIGRATOR_FILE
 DEVTRON_MIGRATOR_LINE=$DEVTRON_MIGRATOR_LINE
 CASBIN_MIGRATOR_LINE=$CASBIN_MIGRATOR_LINE
-
+VERSION_FILE_HYPERION="charts/devtron/values.yaml"
+VERSION_FILE_CHART="charts/devtron/Chart.yaml"
+HYPERION_DEVTRON_MIGRATOR_LINE=$HYPERION_DEVTRON_MIGRATOR_LINE
+HYPERION_CASBIN_MIGRATOR_LINE=$HYPERION_CASBIN_MIGRATOR_LINE
 
 #Getting the commits
 BUILD_COMMIT=$(git rev-parse HEAD)
@@ -65,25 +71,23 @@ fi
 #Updating LTAG Version in the installation-script
 sed -i "s/LTAG=.*/LTAG=\"$NEXT_RELEASE_VERSION\";/" manifests/installation-script
 
-
 #Updating latest installation-script URL in the devtron-installer.yaml
 sed -i "s/url:.*/url: $RAW_GIT_REPO$NEXT_RELEASE_VERSION\/manifests\/installation-script/" manifests/install/devtron-installer.yaml
 
 
 
-
-#IF ELSE for migration
 echo "=================If else check from migration======================="
-if [ [$DEVTRON_MIGRATOR_LINE=="x" && $CASBIN_MIGRATOR_LINE=="x"] ]
+if [[ $DEVTRON_MIGRATOR_LINE == "x" && $CASBIN_MIGRATOR_LINE == "x" ]]
   then
    echo "No Migration Changes"
   else 
 # ========== Updating the Migration script with latest commit hash ==========
+    echo "Migration hash update"
     sed -i "$DEVTRON_MIGRATOR_LINE s/value.*/value: $BUILD_COMMIT/" manifests/yamls/$MIGRATOR_FILE
     sed -i "$CASBIN_MIGRATOR_LINE s/value.*/value: $BUILD_COMMIT/" manifests/yamls/$MIGRATOR_FILE
-#################################
+
 fi
-echo "=================If else check from migration======================="
+echo "=================If else end for migration======================="
 
 git commit -am "Updated latest image of $APP_DOCKER_REPO in the installer"
 git push -f https://$GIT_USERNAME:$GITHUB_TOKENS@$GIT_REPO --all
@@ -91,3 +95,82 @@ git push -f https://$GIT_USERNAME:$GITHUB_TOKENS@$GIT_REPO --all
 
 PR_RESPONSE=$(../bin/gh pr create --title "RELEASE: PR for $NEXT_RELEASE_VERSION" --body "Updates in $APP_DOCKER_REPO micro-service" --base $GIT_BRANCH --head $RELEASE_BRANCH --repo $REPO)
 echo "FINAL PR RESPONSE: $PR_RESPONSE"
+
+
+echo "==============================Hyperion Repo==========================="
+cd ..
+ls
+pwd
+echo "=============================checking files=========================="
+bin/gh repo clone "gunish-dt/hyperionRelease"
+cd hyperionRelease
+git checkout "$GIT_BRANCH"
+git checkout -b "$RELEASE_BRANCH"
+git pull origin "$RELEASE_BRANCH"
+
+
+# sed -i "s/quay.io\/devtron\/$APP_DOCKER_REPO:.*/quay.io\/devtron\/$APP_DOCKER_REPO:$DOCKER_IMAGE_TAG\"/" charts/devtron/$HYPERION_APP_MANIFEST
+
+echo "=================Additional check for orchestrator image change====================="
+
+if [[ $APP_DOCKER_REPO == "devtron" ]]
+ then
+   sed -i "s/quay.io\/devtron\/hyperion:.*/quay.io\/devtron\/hyperion:$DOCKER_IMAGE_TAG\"/" charts/devtron/$HYPERION_APP_MANIFEST
+   echo "hyperion image tag change for orchestrator"
+ else 
+   sed -i "s/quay.io\/devtron\/$APP_DOCKER_REPO:.*/quay.io\/devtron\/$APP_DOCKER_REPO:$DOCKER_IMAGE_TAG\"/" charts/devtron/$HYPERION_APP_MANIFEST
+   echo "$APP_DOCKER_REPO Microservice image tag change for "
+fi
+
+echo "=================End Additional check for orchestrator image change====================="
+
+echo "######################################"
+
+if [[ $DEV_RELEASE == $RELEASE_VERSION ]]
+  then
+    #RELEASE_VERSION=$(../bin/gh release list -L 1 -R $REPO | awk '{print $1}')
+    HYP_RELEASE_VERSION=$(echo ${DEV_RELEASE} | awk -F. -v OFS=. '{$NF++;print}')
+    echo "NEXTVERSION from inside loop: $HYP_RELEASE_VERSION"
+    sed -i "s/$DEV_RELEASE/$HYP_RELEASE_VERSION/" $VERSION_FILE_HYPERION
+  else
+    HYP_RELEASE_VERSION=$DEV_RELEASE
+    echo "NEXTVERSION from inside ESLE: $HYP_RELEASE_VERSION"
+fi
+
+
+echo '===============================Chart Version Change================================='
+
+#Version change in Chart.yaml
+wget https://raw.githubusercontent.com/gunish-dt/hyperionRelease/main/charts/devtron/Chart.yaml -O Chart.yaml
+CHART_DEV_RELEASE=$(sed -nre '13s/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p' Chart.yaml)
+echo $CHART_DEV_RELEASE
+
+CHART_NEXT_RELEASE=$(echo ${CHART_DEV_RELEASE} | awk -F. -v OFS=. '{$NF++;print}')
+
+echo $CHART_NEXT_RELEASE
+
+sed -i "s/$CHART_DEV_RELEASE/$CHART_NEXT_RELEASE/" $VERSION_FILE_CHART
+
+rm Chart.yaml
+
+
+
+echo "====================If else to check microservice for migration changes================="
+
+if [[ $APP_DOCKER_REPO == "devtron" ]]
+  then
+# ========== Updating the Migration script with latest commit hash ==========
+    sed -i "$HYPERION_DEVTRON_MIGRATOR_LINE s/GIT_HASH.*/GIT_HASH: \"$BUILD_COMMIT\"/" charts/devtron/$HYPERION_APP_MANIFEST
+    sed -i "$HYPERION_CASBIN_MIGRATOR_LINE s/GIT_HASH.*/GIT_HASH: \"$BUILD_COMMIT\"/" charts/devtron/$HYPERION_APP_MANIFEST
+  else
+    echo "No Migration change in values.yaml file for hyperion"
+fi
+  
+git commit -am "Updated latest image of $APP_DOCKER_REPO in Values.yaml file"
+git push -f https://$GIT_USERNAME:$GITHUB_TOKENS@$HYPERION_GIT_REPO --all
+#Creating Release ######################
+#RELEASE_RESPONSE=$(../bin/gh release create $NEXT_RELEASE_VERSION --target $RELEASE_BRANCH -R $REPO)
+#echo "FINAL RELEASE RESPONSE: $RELEASE_RESPONSE"
+#Creating PR into main branch
+HYPE_PR_RESPONSE=$(../bin/gh pr create --title "RELEASE: PR for $NEXT_RELEASE_VERSION" --body "Updates in $APP_DOCKER_REPO micro-service" --base $GIT_BRANCH --head $RELEASE_BRANCH --repo $HYPERION_REPO)
+echo "FINAL PR RESPONSE: $HYPE_PR_RESPONSE"
